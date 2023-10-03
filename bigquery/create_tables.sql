a4bffeff3bbdc5b8a51e544974eb1f8d795bbcdb
Marc Galera <mgaleramunoz@gmail.com>
2023-10-03 11:51:54 +0200

    feat: 'add .sql file to create bq tables'

---
+++ bigquery/create_tables.sql
@@ -0,0 +1,20 @@
+# tracker:
+--------
+CREATE TABLE `e3escoles.ordino.tracker_registry` (
+    file_name STRING NOT NULL,
+    num_lines INT64 NOT NULL,
+    tmstmp TIMESTAMP NOT NULL
+)
+PARTITION BY DATE(tmstmp)
+--------
+
+# nationals:
+--------
+CREATE TABLE `tourism-361611.final2.nationals` (
+    imsi STRING NOT NULL,
+    zone STRING NOT NULL,
+    tmstmp TIMESTAMP NOT NULL
+)
+PARTITION BY DATE(tmstmp)
+CLUSTER BY zone;
+--------
