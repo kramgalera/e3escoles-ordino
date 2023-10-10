f84fda4535f1673b95ae7f097d084fb8e94b98d9
Marc Galera <mgaleramunoz@gmail.com>
2023-10-10 17:51:07 +0200

    feat: 'create batch_file function'

---
+++ batch_function_module.py
@@ -0,0 +1,23 @@
+"""Module containing the function that takes data from Cloud Storage and uploads it to Bigquery"""
+
+
+def batch_file(bq_client, config, bucket_name, file_path):
+    """
+    Takes one file from CS and loads its data to BQ
+    
+    Parameters:
+        - bq_client (client): instance of google.cloud.bigquery.Client
+        - config (dict): contains all env vars
+        - bucket_name (str): name of the Cloud Storage bucket
+        - file_path (str): path of the file inserted to BigQuery
+    
+    Returns:
+        - None
+    """
+
+    # Load env vars:
+    project_id = config['project_id']
+    bq_dataset = config['bq_dataset']
+
+
+    return 
