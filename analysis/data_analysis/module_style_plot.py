e0c249a75724a5dc136f3a1cd01893f75b803f1b
Marc Galera <mgalera@ari.af>
2024-01-26 13:19:33 +0100

    feat: add module with dicts of style

---
+++ analysis/data_analysis/module_style_plot.py
@@ -0,0 +1,69 @@
+temp_style_plot = [    
+    dict(
+        type='rect',
+        xref='paper', x0=0, x1=1,
+        yref='y', y0=10, y1=20,
+        fillcolor='lightblue',
+        opacity=0.2,
+        layer='below'
+    ),
+    dict(
+        type='rect',
+        xref='paper', x0=0, x1=1,
+        yref='y', y0=20, y1=26,
+        fillcolor='lightgreen',
+        opacity=0.2,
+        layer='below'
+    ),
+    dict(
+        type='rect',
+        xref='paper', x0=0, x1=1,
+        yref='y', y0=26, y1=30,
+        fillcolor='lightcoral',
+        opacity=0.2,
+        layer='below'
+    )
+]
+
+co2_style_plot = [
+    dict(
+        type='rect',
+        xref='paper', x0=0, x1=1,
+        yref='y', y0=0, y1=400,
+        fillcolor='green',
+        opacity=0.2,
+        layer='below'
+    ),
+    dict(
+        type='rect',
+        xref='paper', x0=0, x1=1,
+        yref='y', y0=400, y1=1000,
+        fillcolor='lightgreen',
+        opacity=0.2,
+        layer='below'
+    ),
+    dict(
+        type='rect',
+        xref='paper', x0=0, x1=1,
+        yref='y', y0=1000, y1=2000,
+        fillcolor='yellow',
+        opacity=0.2,
+        layer='below'
+    ),
+    dict(
+        type='rect',
+        xref='paper', x0=0, x1=1,
+        yref='y', y0=2000, y1=5000,
+        fillcolor='orange',
+        opacity=0.2,
+        layer='below'
+    ),
+    dict(
+        type='rect',
+        xref='paper', x0=0, x1=1,
+        yref='y', y0=5000, y1=8000,
+        fillcolor='lightcoral',
+        opacity=0.2,
+        layer='below'
+    )
+]
