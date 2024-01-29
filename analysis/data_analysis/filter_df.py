ea6ac90b289a07c0b422d9eeeadf01992c59b905
Marc Galera <mgalera@ari.af>
2024-01-29 10:33:05 +0100

    feat: 'add new function to filter vacations'

--- analysis/data_analysis/filter_df.py
+++ analysis/data_analysis/filter_df.py
@@ -17,8 +17,20 @@ def filter_by_schedule (dataframe, start_time='08:30:00', end_time='17:00:00'):
 def filter_weekend (dataframe):
     """Description"""
     df = dataframe.copy()
-    is_weekend = (df['Time(dd/mm/yyyy)'].dt.dayofweek >= 5)  # 5 and 6 represent Saturday and Sunday
+    is_weekend = df['Time(dd/mm/yyyy)'].dt.dayofweek >= 5  # 5 and 6 represent Saturday and Sunday
     df = df[~is_weekend]
     df.reset_index(inplace=True, drop=True)
     return df
 
+def filter_vacations (dataframe, vacations):
+    """Description"""
+    df = dataframe.copy()
+
+    for vacation in vacations:
+        list_tmstmp = pd.to_datetime(vacation, format= '%d/%m/%Y %H:%M:%S')
+        mask_time = (df['Time(dd/mm/yyyy)'] >= list_tmstmp[0]) & (df['Time(dd/mm/yyyy)'] <= list_tmstmp[1])
+        df = df[~mask_time]
+
+    df.reset_index(inplace=True, drop=True)
+    #df.drop(columns=['level_0'], inplace=True)
+    return df
