a043b08d6de379e02a0ed482fb05c92af803ffc8
Marc Galera <mgalera@ari.af>
2024-01-29 09:52:55 +0100

    feat: 'add filtering functions: by school schedule + weekend'

---
+++ analysis/data_analysis/filter_df.py
@@ -0,0 +1,24 @@
+"""Module dedicated to filter the dataframes"""
+
+import pandas as pd
+
+def filter_by_schedule (dataframe, start_time='08:30:00', end_time='17:00:00'):
+    """Description"""
+
+    df = dataframe.copy()
+    df['Temperature(°C)'] = df['Temperature(°C)'].apply(lambda x: float(x.replace(',', '.')) if isinstance(x, str) else x)
+
+    mask_time = (df['Time(dd/mm/yyyy)'].dt.strftime('%H:%M:%S') >= start_time) & (df['Time(dd/mm/yyyy)'].dt.strftime('%H:%M:%S') <= end_time)
+    df_working_hours = df.loc[mask_time]
+    df_working_hours.dropna(axis='rows', how='any', inplace=True)
+    df_working_hours.reset_index(inplace=True, drop=True)
+    return df_working_hours
+
+def filter_weekend (dataframe):
+    """Description"""
+    df = dataframe.copy()
+    is_weekend = (df['Time(dd/mm/yyyy)'].dt.dayofweek >= 5)  # 5 and 6 represent Saturday and Sunday
+    df = df[~is_weekend]
+    df.reset_index(inplace=True, drop=True)
+    return df
+
