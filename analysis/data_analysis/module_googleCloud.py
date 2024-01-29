b8fd24d904829512b1ed41d01a9d31a7cca45728
Marc Galera <mgalera@ari.af>
2024-01-29 09:52:15 +0100

    chore: 'improve code with linter'

--- analysis/data_analysis/module_googleCloud.py
+++ analysis/data_analysis/module_googleCloud.py
@@ -1,28 +1,32 @@
+# pylint: disable = E0401
+
 '''This script is dedicated to establish the connection with the GoogleCloud Servers, and make basic manipulations in BigQuery'''
 
 import os
 from google.cloud import bigquery as bq
 from google.cloud import storage
-from dotenv import load_dotenv
+import dotenv
 
-load_dotenv()
+dotenv.load_dotenv()
 JSON_AUTH_PATH = os.getenv('gcloud_json_key')
 
-def big_query_conn (JSON_AUTH_PATH):
-    '''{{add desctiption}}'''
-    return bq.Client.from_service_account_json(JSON_AUTH_PATH)
+def big_query_conn (json_auth_path):
+    """{{add desctiption}}"""
+    return bq.Client.from_service_account_json(json_auth_path)
 
-def storage_connection (JSON_AUTH_PATH):
-    '''{{add desctiption}}'''
-    return storage.Client.from_service_account_json(JSON_AUTH_PATH)
+def storage_connection (json_auth_path):
+    """{{add desctiption}}"""
+    return storage.Client.from_service_account_json(json_auth_path)
 
 def job_configuration_insert():
+    """{{add desctiption}}"""
     job_config = bq.LoadJobConfig()
     job_config.write_disposition = 'WRITE_TRUNCATE'
     job_config.autodetect = True
     job_config.ignore_unknown_values = True
 
 def remove_duplicates(client, table_id, column):
+    """{{add desctiption}}"""
     # Query to remove duplicates
     query_rm_dupl = f'''
         create or replace table `{table_id}` as (
